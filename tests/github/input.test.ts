a18bd70a1047345c185e28700597b6ab9b38f211
keithalpichi <keithalpichi@gmail.com>
2021-03-11 13:25:14 -0800

    Allow slack message options used as inputs

--- tests/github/input.test.ts
+++ tests/github/input.test.ts
@@ -17,11 +17,16 @@ describe('Inputs', () => {
   test('optional inputs provided to Github Action', () => {
     const inputArgs: InputOptions = {
       template: 'plain1',
-      title: '',
-      description: '',
+      title: 'title',
+      description: 'desc',
       status: 'success',
       channel: '#cicd',
-      steps: '{ "Lint": {"outcome": "success"} }'
+      steps: '{ "Lint": {"outcome": "success"} }',
+      username: 'sharky',
+      icon_emoji: 'abc.com/emoji.png',
+      icon_url: 'abc.com/icon.png',
+      text: 'this is fallback text',
+      link_names: true
     }
     setInputEnvs(inputArgs)
     const i = new Inputs()
@@ -30,5 +35,10 @@ describe('Inputs', () => {
     expect(i.status).toBe(inputArgs.status)
     expect(i.channel).toBe(inputArgs.channel)
     expect(i.steps).toEqual(JSON.parse(inputArgs.steps))
+    expect(i.username).toBe(inputArgs.username)
+    expect(i.icon_emoji).toBe(inputArgs.icon_emoji)
+    expect(i.icon_url).toBe(inputArgs.icon_url)
+    expect(i.text).toBe(inputArgs.text)
+    expect(i.link_names).toBe(inputArgs.link_names)
   })
 })
