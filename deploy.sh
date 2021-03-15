00683d8cac116dd4f7de84525d2c38d2c3757f41
keithalpichi <keithalpichi@gmail.com>
2021-03-14 17:02:29 -0700

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,34 @@
 
 set -euo pipefail
 
+v_type="patch"
+if [[ $# > 0 ]]; then
+  v_type="$1"
+fi
+
+if [[ "$v_type" != "patch" ]] && [[ "$v_type" != "minor" ]] && [[ "$v_type" != "major" ]]; then
+  echo "Please provide 'patch', 'minor', or 'major' to script"
+  exit 1
+fi
+
 echo "Compiling source"
 npm run build
 
-if [[ $(git status | grep 'dist/index.js'; echo $?) == "0" ]]; then
-  echo "Commiting new build"
-  git commit 'new build'
-fi
+echo "Updating $v_type package version"
+npm version --no-git-tag-version "$version"
+git add ./package-lock.json ./package.json
+version=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+git commit -m \""$version"\"
 
 echo "Pushing changes to repository"
 git push origin master
+
+echo "Creating new $version tag"
+git tag "$version"
+echo "Pushing $version tag to repository"
+git push origin "$version"
